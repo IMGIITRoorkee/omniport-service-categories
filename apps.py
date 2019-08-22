6b2b80c53e13e4a1d8c1abbd27f7873e8660db08
Ayush Jain <ayushjain20n@gmail.com>
2019-08-22 16:11:50 +0530

    Change configuration according to new structure

--- apps.py
+++ apps.py
@@ -1,5 +1,3 @@
-from django.apps import AppConfig
+from configuration.utils.app_config_class import get_app_config_class
 
-
-class CategoriesConfig(AppConfig):
-    name = 'categories'
+Config = get_app_config_class(__file__)
